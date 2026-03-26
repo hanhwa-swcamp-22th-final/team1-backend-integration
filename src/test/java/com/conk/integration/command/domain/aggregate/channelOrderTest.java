9fb243255f7d9fce85f1b0659a948209fc779bea
playdata\playdata2 <gusgh075@gmail.com>
2026-03-26 16:16:33 +0900

    dev profile gitignore 설정

---
+++ src/test/java/com/conk/integration/command/domain/aggregate/channelOrderTest.java
@@ -0,0 +1,8 @@
+package com.conk.integration.command.domain.aggregate;
+
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+@DataJpaTest
+public class channelOrderTest {
+
+}
